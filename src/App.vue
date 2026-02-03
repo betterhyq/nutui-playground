cf06517576cb19d39c0521280c79f73c7aec45dc
heyongqi10 <heyongqi10@jd.com>
2026-02-03 14:21:22 +0800

    feat: Refactor code structure for improved readability and maintainability

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
+import { showToast } from '@nutui/nutui'
+const show = () => {
+  showToast.text('Hello NutUI Toast!')
+};
 </script>
 
 <template>
   <div>
-    <nut-button type="primary">NutUI Button</nut-button>
+    <nut-button type="primary" @click="show">NutUI Button</nut-button>
   </div>
 </template>
 
